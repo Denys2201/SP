e258841309ad31339acb8fa488b1302d048dcbf2
Denys Chornobai <73062854+Denys2201@users.noreply.github.com>
2020-10-18 18:15:40 +0300

    lab2(project)
    18.10.2020

---
+++ lab2/lab2(sp)/lab2(sp)/lab2(sp).cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+
+#include <stdio.h>
+
+extern "C" int calc(int, int, int);
+
+int main(){
+	int res = 0;
+	int b = 0;
+	int d = 0;
+	int e = 0;
+	printf("B = ");
+	scanf_s("%d", &b);
+	printf("D = ");
+	scanf_s("%d", &d);
+	printf("E = ");
+	scanf_s("%d", &e);
+
+	res = calc(b, d, e);
+	printf("Result: %d", res);
+	return 0;
+}
